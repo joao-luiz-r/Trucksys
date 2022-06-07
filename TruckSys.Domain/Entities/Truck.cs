59b200f97a9f107d4d71d14e1118d65db270c5fe
João Luiz Rodrigues <joaoluizr@gmail.com>
2022-06-07 14:58:38 -0300

    Add project files.

---
+++ TruckSys.Domain/Entities/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TruckSys.Models
+{
+    [Table("Truck")]
+    public class Truck
+    {
+        [Key]
+        [Column("Id")]
+        [Display(Name ="Código")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("Modelo", TypeName = "varchar(2)")]
+        [Display(Name = "Modelo")]
+        public string Modelo { get; set; }
+
+        [Column("AnoFabricacao")]
+        [Display(Name = "Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Column("AnoModelo")]
+        [Display(Name = "Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
